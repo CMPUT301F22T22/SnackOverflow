bff76e09798305e399c329ba6c411750f8f8e4c3
c1r <76684950+c1r@users.noreply.github.com>
2022-10-25 21:02:37 -0600

    fixed Recipe class, removed test stuff in RecipeActivity.java

--- code/app/src/main/java/com/example/snackoverflow/RecipeActivity.java
+++ code/app/src/main/java/com/example/snackoverflow/RecipeActivity.java
@@ -6,9 +6,7 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class RecipeActivity extends AppCompatActivity {
     ListView recipeList;
@@ -20,7 +18,7 @@ public class RecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
-        recipeList = findViewById(R.id.recipeListView);
+        recipeList = findViewById(R.id.recipe_list);
         recipeDataList = new ArrayList<>();
 
         recipeArrayAdapter = new RecipeAdapter(this, recipeDataList);
