a2a1a3f1af23e3510c436752ad57324638ea45b2
c1r <76684950+c1r@users.noreply.github.com>
2022-10-25 20:17:29 -0600

    Revert "Revert "Added RecipeActivity.java and its xml, fixed recipe class""
    This reverts commit 954e26626dcd6075088666b58d8c33dfd690e850.

--- code/app/src/main/java/com/example/snackoverflow/MainActivity.java
+++ code/app/src/main/java/com/example/snackoverflow/MainActivity.java
@@ -11,32 +11,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    ListView recipeList;
-    ArrayAdapter<Recipe> recipeArrayAdapter;
-    ArrayList<Recipe> recipeDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        recipeList = findViewById(R.id.recipeListView);
-        recipeDataList = new ArrayList<>();
-        String []recipestitle = {"Curry","NOODLES"};
-        int [] servings = {1,2};
-        String []categories = {"Lunch","Dinner"};
-
-
-        //Test Data
-        for (int i =0;i<recipestitle.length;i++){
-            recipeDataList.add(new Recipe(recipestitle[i], LocalTime.now(),2.0f,"Lunch","HAHA",new ArrayList<String>(Arrays.asList(new String[]{"Nidal","Nasemm"}))));
-        }
-
-
-
-        recipeArrayAdapter = new RecipeAdapter(this,recipeDataList);
-        recipeList.setAdapter(recipeArrayAdapter);
-
-
     }
 }
