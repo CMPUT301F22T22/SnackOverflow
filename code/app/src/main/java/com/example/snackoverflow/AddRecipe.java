c4bbb5e060990b59b24d880f8c48b9df9e572d82
Nagham Eldegwy <naghamhesham27@gmail.com>
2022-10-26 02:17:44 -0600

    use more descriptive IDs

--- code/app/src/main/java/com/example/snackoverflow/AddRecipe.java
+++ code/app/src/main/java/com/example/snackoverflow/AddRecipe.java
@@ -25,7 +25,7 @@ public class AddRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
-        imageView = findViewById(R.id.addPhoto);
+        imageView = findViewById(R.id.recipe_addPhoto);
 
         // Register activity result to handle the Image the user selected
         ActivityResultLauncher selectImage = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
