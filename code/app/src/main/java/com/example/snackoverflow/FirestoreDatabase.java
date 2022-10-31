bb2241695fd8c4f5bd6c4e68e14ec4099b5eaadd
gokul887 <sivadasa@ualberta.ca>
2022-10-30 20:40:12 -0600

    fix: database class

---
+++ code/app/src/main/java/com/example/snackoverflow/FirestoreDatabase.java
@@ -0,0 +1,43 @@
+package com.example.snackoverflow;
+
+public abstract class FirestoreDatabase {
+
+    FirestoreDatabase db;
+
+    void addIngredient() {};
+
+    void modifyIngredient() {};
+
+    void deleteIngredient() {};
+
+    void fetchIngredients() {};
+
+    void addRecipe() {};
+
+    void modifyRecipe() {};
+
+    void deleteRecipe() {};
+
+    void fetchRecipes() {};
+
+    void addMealPlan() {};
+
+    void modifyMealPlan() {};
+
+    void deleteMealPlan() {};
+
+    void fetchMealPlans() {};
+
+    void addShoppingList() {};
+
+    void addToShoppingList() {};
+
+    void modifyShoppingList() {};
+
+    void deleteShoppingList() {};
+
+    void deleteFromShoppingList() {};
+
+    void fetchShoppingList() {};
+
+}
