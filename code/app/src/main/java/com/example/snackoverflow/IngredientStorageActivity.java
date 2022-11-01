7e54632fa3b54096cad00aa4c83dd89d6ea39d57
Nagham Eldegwy <naghamhesham27@gmail.com>
2022-11-01 01:29:59 -0600

    fix typo

--- code/app/src/main/java/com/example/snackoverflow/IngredientStorageActivity.java
+++ code/app/src/main/java/com/example/snackoverflow/IngredientStorageActivity.java
@@ -24,7 +24,7 @@ public class IngredientStorageActivity extends AppCompatActivity implements AddI
     private ArrayAdapter<Ingredient> ingredientArrayAdapter;
     private ArrayList<Ingredient> ingredients;
 
-    // TODO: Maybe change category to radio buttons for user to select
+    // TODO: Maybe change location to radio buttons for user to select
     //TODO: fix best before display as it only shows null now
     @Override
     protected void onCreate(Bundle savedInstanceState) {
