664c989811b12f98cc5fce72fc3bb3b2989426e6
Nagham Eldegwy <naghamhesham27@gmail.com>
2022-11-02 03:26:56 -0600

    add firestore functions to add/delete/fetch ingredients

--- code/app/src/main/java/com/example/snackoverflow/FirestoreDatabase.java
+++ code/app/src/main/java/com/example/snackoverflow/FirestoreDatabase.java
@@ -1,11 +1,14 @@
 package com.example.snackoverflow;
 
 import android.util.Log;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -13,6 +16,10 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+<<<<<<< HEAD
+=======
+import java.util.Date;
+>>>>>>> eaee606 (add firestore functions to add/delete/fetch ingredients)
 import java.util.Map;
 
 public class FirestoreDatabase {
@@ -21,30 +28,85 @@ public class FirestoreDatabase {
     final static CollectionReference recipeCol = db.collection("recipe");
     final static CollectionReference ingredientsCol = db.collection("ingredient");
 
-    private final static String IngredeintsTAG = "IngredientStorageActivity";
+    private final static String IngredientsTAG = "IngredientStorageActivity";
 
     static void addIngredient(Ingredient newIngredient) {
-                ingredientsCol
-                .add(newIngredient)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(IngredeintsTAG, "Ingredient DocumentSnapshot written with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(IngredeintsTAG, "Error adding ingredient document", e);
-                    }
-                });
+        ingredientsCol
+        .add(newIngredient)
+        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(IngredientsTAG, "Ingredient document snapshot written with ID: " + documentReference.getId());
+                newIngredient.id = documentReference.getId();
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(IngredientsTAG, "Error adding ingredient document", e);
+            }
+        });
     };
 
     static void modifyIngredient() {};
 
-    static void deleteIngredient() {};
+    static void deleteIngredient(Ingredient ingredient) {
+        ingredientsCol
+        .document(ingredient.id)
+        .delete()
+        .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(IngredientsTAG, "Ingredient document snapshot successfully deleted!");
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(IngredientsTAG, "Error deleting ingredient document", e);
+            }
+        });
+    };
 
-    static void fetchIngredients() {};
+    static void fetchIngredients(ArrayAdapter<Ingredient> ingredientArrayAdapter) {
+        ingredientsCol
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(IngredientsTAG, "Ingredients retrieved successfully");
+//                            this.title = description;
+//                            this.location = location;
+//                            this.amount = amount;
+//                            this.unit = unit;
+//                            this.bestBefore = date;
+//                            this.category = category;
+//public Ingredient(String description, Date date, String location, int amount, int unit, String category)
+                            ArrayList<Ingredient> allIngredients = new ArrayList<>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String title = document.getString("title");
+                                String location = document.getString("location");
+                                int amount = document.getLong("amount").intValue();
+                                int unit = document.getLong("unit").intValue();
+                                Date bestBefore = document.getDate("bestBefore");
+                                String category = document.getString("category");
+                                System.out.println(title);
+                                System.out.println(location);
+                                System.out.println(amount);
+                                System.out.println(unit);
+                                System.out.println(bestBefore);
+                                System.out.println(category);
+
+                                Ingredient ingredient = new Ingredient(title, bestBefore, location, amount, unit, category);
+                                allIngredients.add(ingredient);
+                            }
+                            ingredientArrayAdapter.clear();
+                            ingredientArrayAdapter.addAll(allIngredients);
+                        }
+                    }
+                });
+    };
 
     static void addRecipe() {};
 
