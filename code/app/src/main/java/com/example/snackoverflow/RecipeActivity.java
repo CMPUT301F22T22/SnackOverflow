976fd87a22e875d11dbe3588be9fe277358e8ec9
TheLastWonderor <71833799+Hitarth-Kothari@users.noreply.github.com>
2022-11-02 18:57:47 -0600

    button alligned

--- code/app/src/main/java/com/example/snackoverflow/RecipeActivity.java
+++ code/app/src/main/java/com/example/snackoverflow/RecipeActivity.java
@@ -81,8 +81,7 @@ public class RecipeActivity extends AppCompatActivity {
         modifyRecipeTestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RecipeActivity.this, AddRecipe.class);
-//                Intent intent = new Intent(RecipeActivity.this, AddRecipe.class);
+                Intent intent = new Intent(RecipeActivity.this, ModifyRecipe.class);
                 startActivity(intent);
             }
         });
