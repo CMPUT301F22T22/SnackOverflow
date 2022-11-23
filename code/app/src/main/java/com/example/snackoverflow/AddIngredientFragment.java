52531aeb4824a317e340dd209a6c775f6e0218dd
Bhavnoor Kaur <bhavnoorks@gmail.com>
2022-11-22 20:33:03 -0700

    Updated Add Ingredient to Storage functionality

--- code/app/src/main/java/com/example/snackoverflow/AddIngredientFragment.java
+++ code/app/src/main/java/com/example/snackoverflow/AddIngredientFragment.java
@@ -1,6 +1,7 @@
 package com.example.snackoverflow;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -9,15 +10,21 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -27,11 +34,14 @@ import java.util.Date;
  * @see Ingredient
  * */
 public class AddIngredientFragment extends DialogFragment {
+    private TextInputLayout ingredientDescLayout;
+    private TextInputLayout ingredientCategoryLayout;
     private EditText ingredientDesc;
     private EditText ingredientAmount;
     private EditText ingredientUnit;
     private EditText ingredientBestBefore;
-    private EditText ingredientLocation;
+    private RadioButton ingredientLocation;
+    private RadioGroup locationRadioGroup;
     private EditText ingredientCategory;
     private OnFragmentInteractionListener listener;
 
@@ -63,45 +73,83 @@ public class AddIngredientFragment extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.add_ingredient_fragment_layout, null);
+        ingredientDescLayout = view.findViewById(R.id.ingredient_description_layout);
+        ingredientCategoryLayout = view.findViewById(R.id.ingredient_category_layout);
         ingredientDesc = view.findViewById(R.id.ingredient_description_editText);
         ingredientBestBefore = view.findViewById(R.id.ingredient_bestBefore_editText);
-        ingredientLocation = view.findViewById(R.id.ingredient_location_editText);
+        locationRadioGroup = view.findViewById(R.id.ingredient_location_radioGroup);
         ingredientAmount = view.findViewById(R.id.ingredient_amount_editText);
         ingredientUnit = view.findViewById(R.id.ingredient_unit_editText);
         ingredientCategory = view.findViewById(R.id.ingredient_category_editText);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 
-        ingredientBestBefore.addTextChangedListener(new TextWatcher() {
+        ingredientDesc.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                //Is_Valid_date(foodBestBefore);
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {
-                isValidDate(ingredientBestBefore);
+            public void afterTextChanged(Editable s) {
+
+                if (s.length() > ingredientDescLayout.getCounterMaxLength()) {
+                    ingredientDescLayout.setError("Max character length is " + ingredientDescLayout.getCounterMaxLength());
+                } else {
+                    ingredientDescLayout.setError(null);
+                }
             }
         });
 
-        ingredientLocation.addTextChangedListener(new TextWatcher() {
+        ingredientBestBefore.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // on below line we are setting date to our edit text.
+                                ingredientBestBefore.setText(year + "-" + (monthOfYear+1) + "-" + dayOfMonth);
+                            }
+                        },
+                        year, month, day);
+
+                datePickerDialog.getDatePicker().setMinDate(new Date().getTime());
+                datePickerDialog.show();
+            }
+        });
+
+        ingredientCategory.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {
-                isValidLocation(ingredientLocation);
+            public void afterTextChanged(Editable s) {
+
+                if (s.length() > ingredientCategoryLayout.getCounterMaxLength()) {
+                    ingredientCategoryLayout.setError("Max character length is " + ingredientCategoryLayout.getCounterMaxLength());
+                } else {
+                    ingredientCategoryLayout.setError(null);
+                }
             }
         });
 
@@ -112,46 +160,26 @@ public class AddIngredientFragment extends DialogFragment {
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // TODO: make variable names more descriptive
                         String ingredientDescString = ingredientDesc.getText().toString();
-                        Integer ingredientAmountInt = (int) Math.ceil(Double.parseDouble(ingredientAmount.getText().toString()));
+                        Integer ingredientAmountInt = (int) Double.parseDouble(ingredientAmount.getText().toString());
                         Integer ingredientUnitInt = Integer.parseInt(ingredientUnit.getText().toString());
+                        String ingredientBestBeforeString = ingredientBestBefore.getText().toString();
                         Date ingredientBestBeforeDate = null;
                         try {
-                            ingredientBestBeforeDate = dateFormat.parse(ingredientBestBefore.getText().toString());
-                        }
-                        catch (ParseException e) {
+                            ingredientBestBeforeDate = dateFormat.parse(ingredientBestBeforeString);
+                        } catch (ParseException e) {
                             e.printStackTrace();
                         }
 
+                        int selectedLocation = locationRadioGroup.getCheckedRadioButtonId();
+                        ingredientLocation = (RadioButton) view.findViewById(selectedLocation);
                         String ingredientLocationString = ingredientLocation.getText().toString();
+
                         String ingredientCategoryString = ingredientCategory.getText().toString();
+
                         listener.onOkPressed(new Ingredient(ingredientDescString, ingredientBestBeforeDate, ingredientLocationString, ingredientUnitInt, ingredientAmountInt, ingredientCategoryString));
                     }
                 }).create();
     }
 
-    /**
-     * Validates the best before date inputted
-     * @param edt text to validate
-     * */
-    private void isValidDate(EditText edt) {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            String formattedDate = dateFormat.format(dateFormat.parse(edt.getText().toString()));
-        } catch (ParseException e) {
-            edt.setError("Format: yyyy-mm-dd");
-        }
-    }
-
-    /**
-     * Validates the location inputted
-     * @param edt text to validate
-     * */
-    private void isValidLocation(EditText edt) {
-        String txt = edt.getText().toString();
-        if (!txt.matches("Pantry|Freezer|Fridge")) {
-            edt.setError("One of Pantry, Fridge, or Freezer");
-        }
-    }
 }
