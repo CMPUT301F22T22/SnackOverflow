969898a55e9b3f288210695ce86ae8ae2136bd70
Bhavnoor Kaur <bhavnoorks@gmail.com>
2022-11-23 21:12:24 -0700

    Added Sort functionality for ingredient storage

--- code/app/src/main/java/com/example/snackoverflow/SortComparator.java
+++ code/app/src/main/java/com/example/snackoverflow/SortComparator.java
@@ -1,7 +1,5 @@
 package com.example.snackoverflow;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 
 public class SortComparator {
@@ -11,6 +9,9 @@ public class SortComparator {
             String objectType = o1.getClass().toString();
             if (objectType.substring(objectType.lastIndexOf('.')+1).equals("Recipe")) {
                 return ((Recipe) o1).getTitle().compareTo(((Recipe)o2).getTitle());
+            }
+            if (objectType.substring(objectType.lastIndexOf('.')+1).equals("Ingredient")) {
+                return ((Ingredient) o1).getTitle().compareTo(((Ingredient)o2).getTitle());
             }
             return -1;
         }
@@ -29,6 +30,9 @@ public class SortComparator {
             String objectType = o1.getClass().toString();
             if (objectType.substring(objectType.lastIndexOf('.')+1).equals("Recipe")) {
                 return ((Recipe) o1).getRecipeCategory().compareTo(((Recipe)o2).getRecipeCategory());
+            }
+            if (objectType.substring(objectType.lastIndexOf('.')+1).equals("Ingredient")) {
+                return ((Ingredient) o1).getCategory().compareTo(((Ingredient)o2).getCategory());
             }
             return -1;
         }
@@ -73,5 +77,41 @@ public class SortComparator {
         public Comparator<Object> reversed() {
             return Comparator.super.reversed();
         }
+    }
+
+    public static class BestBeforeComparator implements Comparator<Object> {
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            String objectType = o1.getClass().toString();
+            if (objectType.substring(objectType.lastIndexOf('.')+1).equals("Ingredient")) {
+                return ((Ingredient) o1).getBestBefore().compareTo(((Ingredient)o2).getBestBefore());
+            }
+            return -1;
+        }
+
+        @Override
+        public Comparator<Object> reversed() {
+            return Comparator.super.reversed();
+        }
+
+    }
+
+    public static class LocationComparator implements Comparator<Object> {
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            String objectType = o1.getClass().toString();
+            if (objectType.substring(objectType.lastIndexOf('.')+1).equals("Ingredient")) {
+                return ((Ingredient) o1).getLocation().compareTo(((Ingredient)o2).getLocation());
+            }
+            return -1;
+        }
+
+        @Override
+        public Comparator<Object> reversed() {
+            return Comparator.super.reversed();
+        }
+
     }
 }
