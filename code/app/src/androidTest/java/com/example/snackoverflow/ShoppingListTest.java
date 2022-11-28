1abb4264c940a26cb6527bb498ee88a230b39ee0
Nagham Eldegwy <naghamhesham27@gmail.com>
2022-11-27 21:58:51 -0700

    fix shopping list UI tests

--- code/app/src/androidTest/java/com/example/snackoverflow/ShoppingListTest.java
+++ code/app/src/androidTest/java/com/example/snackoverflow/ShoppingListTest.java
@@ -10,12 +10,8 @@ import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import org.junit.runners.MethodSorters;
 
 import android.app.Activity;
 import android.view.View;
@@ -64,37 +60,14 @@ public class ShoppingListTest {
         solo.clickOnView(addButton);
         EditText title =  (EditText) solo.getView(R.id.ingredient_description_editText);
         String titleText = title.getText().toString();
-
         solo.clickOnRadioButton(0);
+
         View dateTextBox = solo.getView(R.id.ingredient_bestBefore_editText);
         solo.clickOnView(dateTextBox);
-        solo.sleep(1500);
-        solo.setDatePicker(0, 2025, 1, 1);
-        solo.clickOnButton(3);
-        solo.sleep(2000);
-        System.out.println("these are my btns");
-        System.out.println(solo.getButton(0));
-        System.out.println(solo.getButton(1));
-        System.out.println(solo.getButton(2));
-        System.out.println(solo.getButton(3));
-        System.out.println(solo.getButton(4));
-        System.out.println(solo.getButton(5));
-        System.out.println(solo.getButton(6));
-        System.out.println(solo.getButton(7));
-        System.out.println(solo.getButton(8));
-        System.out.println(solo.getButton(9));
-        System.out.println(solo.getButton(10));
-        System.out.println(solo.getButton(11));
-        System.out.println(solo.getButton(12));
-        System.out.println(solo.getButton(13));
-        System.out.println(solo.getButton(14));
-        System.out.println(solo.getButton(15));
-        System.out.println(solo.getButton(16));
-        System.out.println(solo.getButton(17));
-        //System.out.println(solo.getButton(-1));
+        solo.clickOnButton("Cancel");
 
         solo.clickOnButton("OK");
-        solo.sleep(2000);
+        solo.sleep(1000);
         Assert.assertFalse(solo.waitForText(titleText, 1, 2000));
     }
 
